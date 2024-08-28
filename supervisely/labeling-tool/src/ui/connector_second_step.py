65ef4956253e4f8ad1d93058147c734cd1523e01
Pavel Bartsits <48913536+cxnt@users.noreply.github.com>
2024-08-28 16:45:42 +0400

    Update connector_second_step.py

--- supervisely/labeling-tool/src/ui/connector_second_step.py
+++ supervisely/labeling-tool/src/ui/connector_second_step.py
@@ -38,6 +38,7 @@ def connect_to_calculator(api: sly.Api, task_id, context, state, app_logger):
         task_id = state['calculatorId']
         response = api.task.send_request(task_id, "get_info", data={}, timeout=3)
         calculator_info = response
+        print(calculator_info)
 
         if calculator_info['Model'] != g.model_info['Model']:
             raise ValueError('Metric Learning model and AI Recommendations model must be the same!')
